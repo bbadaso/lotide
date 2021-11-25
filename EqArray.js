e58154e12ecdd2f6681fd03edf59729531e59ded
bbadaso <badasobarento@gmail.com>
2021-11-25 03:01:39 +0000

    adding eqArray

--- EqArray.js
+++ EqArray.js
@@ -25,5 +25,6 @@ const eqArrays = function(a1, a2)
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
 // assertEqual(eqArrays(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+//
 
 module.exports = eqArrays;
